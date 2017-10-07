c8b18849bee8f7e8badce5a4413baf9def8e1eb0
Sam Triest <sam.triest@gmail.com>
2017-10-07 23:57:55 +0200

    Add class User to models.py

---
+++ final-project/data/models.py
@@ -0,0 +1,14 @@
+from data.database import Base, create_table_metadata
+from sqlalchemy import Column, String, Integer
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
+    def __repr__(self):
+        return "<User: (username = {}, password = {})>".format(self.username, self.password)
+
