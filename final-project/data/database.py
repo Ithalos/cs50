8cdd2b7fd405b49ff51aee712de4c5ebec59d65e
Sam Triest <sam.triest@gmail.com>
2017-10-07 22:59:21 +0200

    Add database.py to data module

---
+++ final-project/data/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+engine = create_engine("sqlite:///database.db", echo=True)
+Session = sessionmaker(bind=engine)
+
+def create_table_metadata():
+    Base.metadata.create_all(engine)
+
