16afb45ce5368f1c2385f113c39e1b2c4612bcf8
Sam Triest <sam.triest@gmail.com>
2017-10-09 23:04:36 +0200

    Add route for login

--- final-project/application.py
+++ final-project/application.py
@@ -13,5 +13,12 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    return render_template("login.html")
 
