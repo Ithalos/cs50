c2156d9407f5734968ea9a03d1563ea50a669e12
Sam Triest <sam.triest@gmail.com>
2017-10-12 21:22:37 +0200

    Add Memo class in models.py

--- final-project/data/models.py
+++ final-project/data/models.py
@@ -1,5 +1,6 @@
 from data.database import Base, create_table_metadata
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 class User(Base):
 
@@ -9,8 +10,23 @@ class User(Base):
     username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
 
+    memos = relationship("Memo")
+
     def __repr__(self):
         return "<User: (username = {}, password = {})>".format(self.username, self.password)
+
+
+class Memo(Base):
+
+    __tablename__ = "memos"
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String, nullable=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    def __repr__(self):
+        return "<Memo: (text = {})>".format(self.text)
 
 
 create_table_metadata()
