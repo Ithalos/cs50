4504e00f0110a8e2308704eaba52a9fcc1390a76
Sam Triest <sam.triest@gmail.com>
2017-10-14 00:33:12 +0200

    Fix login_required checking wrong session variable

--- final-project/helpers.py
+++ final-project/helpers.py
@@ -16,7 +16,7 @@ def login_required(f):
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user") is None:
+        if session.get("user_id") is None:
             return redirect(url_for("login", next=request.url))
         return f(*args, **kwargs)
     return decorated_function
