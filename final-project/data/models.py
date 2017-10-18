adcf1e8f555f87765b6366f1d916b76e555bfb66
Sam Triest <sam.triest@gmail.com>
2017-10-18 18:30:50 +0200

    Add Task class to models.py

--- final-project/data/models.py
+++ final-project/data/models.py
@@ -1,5 +1,5 @@
 from data.database import Base, create_table_metadata
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, Date, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 class User(Base):
@@ -11,6 +11,7 @@ class User(Base):
     password = Column(String, nullable=False)
 
     memos = relationship("Memo", back_populates="user", cascade="all, delete, delete-orphan")
+    tasks = relationship("Task", back_populates="user", cascade="all, delete, delete-orphan")
 
     def __repr__(self):
         return "<User: (username = {}, password = {})>".format(self.username, self.password)
@@ -28,6 +29,21 @@ class Memo(Base):
 
     def __repr__(self):
         return "<Memo: (text = {})>".format(self.text)
+
+
+class Task(Base):
+
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    text = Column(String, nullable=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="tasks")
+
+    def __repr__(self):
+        return "<Task: (date = {}, text = {})>".format(self.date, self.text)
 
 
 create_table_metadata()
