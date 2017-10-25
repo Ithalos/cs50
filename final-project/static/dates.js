7849a28208c1e051c601bac279995ac75b1bccb5
Sam Triest <sam.triest@gmail.com>
2017-10-25 18:14:08 +0200

    Add getDateDifference to dates.js

--- final-project/static/dates.js
+++ final-project/static/dates.js
@@ -1,3 +1,13 @@
+/*
+ * Returns the difference between two dates, in days.
+*/
+function getDateDifference(today, entry)
+{
+    // Subtracting two dates returns a value in milliseconds.
+    // 1000 * 60 * 60 * 24 is the amount of milliseconds in one day.
+    return Math.floor((entry - today) / (1000 * 60 * 60 * 24)) + 1;
+}
+
 function getDates()
 {
     return document.getElementsByClassName("js_date");
