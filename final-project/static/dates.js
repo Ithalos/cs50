919721ff03112640d3081a9c50b172f943f16b4c
Sam Triest <sam.triest@gmail.com>
2017-10-25 18:21:06 +0200

    Add setTaskDates to dates.js

--- final-project/static/dates.js
+++ final-project/static/dates.js
@@ -9,6 +9,36 @@ window.onload = function()
 }
 
 /*
+ * Populates the table data elements for days left in /tasks
+*/
+function setTaskDates(dates, daysLeft)
+{
+    var today = new Date();
+
+    for (var i = 0; i < dates.length; i++)
+    {
+        var entry = new Date(dates[i].innerHTML);
+        var diff = getDateDifference(today, entry);
+
+        if (diff > 0)
+        {
+            daysLeft[i].style.color = "green";
+            daysLeft[i].innerHTML = diff;
+        }
+        else if (diff < 0)
+        {
+            daysLeft[i].style.color = "red";
+            daysLeft[i].innerHTML = "Passed";
+        }
+        else
+        {
+            daysLeft[i].style.color = "orange";
+            daysLeft[i].innerHTML = "Today";
+        }
+    }
+}
+
+/*
  * Returns the difference between two dates, in days.
 */
 function getDateDifference(today, entry)
