364e7dd45681f8cca364091f501d30f03076c01a
Sam Triest <sam.triest@gmail.com>
2017-12-11 21:29:10 +0100

    Add comment

--- final-project/data/database.py
+++ final-project/data/database.py
@@ -6,6 +6,7 @@ Base = declarative_base()
 engine = create_engine("sqlite:///database.db", echo=True)
 Session = sessionmaker(bind=engine)
 
+# Create metadata for database tables
 def create_table_metadata():
     Base.metadata.create_all(engine)
 
